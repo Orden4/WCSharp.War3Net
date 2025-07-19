92ad764bdd63d92361e1a2765488f45848f063ac
Orden4 <erxos479@gmail.com>
2025-07-19 18:59:43 +0200

    Remove StyleCop. Modify some projects for Github workflows. Add a tool for pushing to NuGet.

---
+++ src/Tools.NuGetPusher/AppSettings.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace NuGetPusher
+{
+	internal class AppSettings
+	{
+		public string Author { get; set; } = null!;
+		public string NuGetSource { get; set; } = null!;
+		public int RetryLimit { get; set; }
+		public int RetryDelay { get; set; }
+		public List<string> Projects { get; set; } = new();
+
+		public string ApiKey { get; set; } = null!;
+		public string PackageDirectory { get; set; } = null!;
+	}
+}
---
+++ src/Tools.NuGetPusher/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Serilog;
+
+namespace NuGetPusher
+{
+	internal class Program
+	{
+		[STAThread]
+		private static async Task Main(string[] args)
+		{
+			Log.Logger = new LoggerConfiguration()
+				.WriteTo.Console()
+				.CreateLogger();
+
+			var configuration = new ConfigurationBuilder()
+				.AddJsonFile("appSettings.json")
+				.AddCommandLine(args)
+				.Build();
+			var appSettings = configuration.Get<AppSettings>()
+				?? throw new InvalidOperationException("No AppSettings found.");
+
+			var runner = new Runner(appSettings);
+			foreach (var project in appSettings.Projects)
+			{
+				await runner.Run(project);
+			}
+		}
+	}
+}
